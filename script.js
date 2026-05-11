7c7de82daf3fa9541b5bdbe2e8bffc20259343f8
Iroha Konishi <irokoni.168@gmail.com>
2026-05-11 14:36:40 -0700

    Redesign project website with HTML, CSS, and JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
 
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
